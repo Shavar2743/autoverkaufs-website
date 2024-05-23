0c1e612ef4d0181901d5aa36522ceb274305c15f
Shavar2743 <markus.shavar@web.de>
2024-05-23 21:04:12 +0200

    homework

--- src/components/autoliste.js
+++ src/components/autoliste.js
@@ -1,13 +1,33 @@
-import bmw from '../bilder/bmw.jpeg'
-import mercedes from '../bilder/mercedes.jpeg'
-import opel from '../bilder/opel.jpeg'
-import bentley from '../bilder/bentley.jpeg'
+import bmw from '../bilder/bmw1.jpeg'
+import mercedes from '../bilder/mercedes1.jpeg'
+import opel from '../bilder/opel1.jpeg'
+import bentley from '../bilder/bentley1.jpeg'
+import audi from '../bilder/audi1.jpeg'
+import vw from '../bilder/vw1.jpeg'
+import toyota from '../bilder/toyota1.jpeg'
+import ford from '../bilder/ford1.jpeg'
+import honda from '../bilder/honda1.jpeg'
+import mazda from '../bilder/mazda1.jpeg'
+import renault from '../bilder/renault1.jpeg'
+import peugeot from '../bilder/peugeot1.jpeg'
 
 const autoliste = [
     {Hersteller:"BMW", Modell:"120i", PS:"170", Farbe:"Schwarz", Baujahr:"2007", Kilometer:"185.000", Preis:"6.700 €", Bild:bmw},
     {Hersteller:"Mercedes", Modell:"A 250", PS:"224", Farbe:"Schwarz", Baujahr:"2019", Kilometer:"45.000", Preis:"29.000 €", Bild:mercedes},
     {Hersteller:"Opel", Modell:"Corsa", PS:"69", Farbe:"Weiß", Baujahr:"2009", Kilometer:"90.000", Preis:"3.000 €", Bild:opel},
     {Hersteller:"Bentley", Modell:"Bentayga", PS:"450", Farbe:"Schwarz", Baujahr:"2019", Kilometer:"59.000", Preis:"130.000 €", Bild:bentley},
+    {Hersteller:"Audi", Modell:"A4", PS:"150", Farbe:"Anthrazit", Baujahr:"2015", Kilometer:"80.000", Preis:"15.500 €", Bild:audi},
+    {Hersteller:"Volkswagen", Modell:"Golf", PS:"110", Farbe:"Rot", Baujahr:"2012", Kilometer:"120.000", Preis:"8.000 €", Bild:vw},
+    {Hersteller:"Toyota", Modell:"Yaris", PS:"100", Farbe:"Schwarz", Baujahr:"2018", Kilometer:"35.000", Preis:"10.200 €", Bild:toyota},
+    {Hersteller:"Ford", Modell:"Focus", PS:"125", Farbe:"Grau", Baujahr:"2016", Kilometer:"70.000", Preis:"12.000 €", Bild:ford},
+    {Hersteller:"Honda", Modell:"Civic", PS:"182", Farbe:"Balu", Baujahr:"2017", Kilometer:"50.000", Preis:"17.000 €", Bild:honda},
+    {Hersteller:"Mazda", Modell:"3", PS:"120", Farbe:"Rot", Baujahr:"2014", Kilometer:"95.000", Preis:"11.000 €", Bild:mazda},
+    {Hersteller:"Renault", Modell:"Clio", PS:"90", Farbe:"Blau", Baujahr:"2020", Kilometer:"25.000", Preis:"14.000 €", Bild:renault},
+    {Hersteller:"Peugeot", Modell:"208", PS:"110", Farbe:"Schwarz", Baujahr:"2019", Kilometer:"30.000", Preis:"13.500 €", Bild:peugeot},
+
+
+
+
   ]
 
   export default autoliste;
