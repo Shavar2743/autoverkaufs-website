90852d5d79e5394526227328c9ce9c4b18576387
Shavar2743 <markus.shavar@web.de>
2024-05-23 12:49:10 +0200

    optimiert

---
+++ src/components/autoliste.js
@@ -0,0 +1,13 @@
+import bmw from '../bilder/bmw.jpeg'
+import mercedes from '../bilder/mercedes.jpeg'
+import opel from '../bilder/opel.jpeg'
+import bentley from '../bilder/bentley.jpeg'
+
+const autoliste = [
+    {Hersteller:"BMW", Modell:"120i", PS:"170", Farbe:"Schwarz", Baujahr:"2007", Kilometer:"185.000", Preis:"6.700 €", Bild:bmw},
+    {Hersteller:"Mercedes", Modell:"A 250", PS:"224", Farbe:"Schwarz", Baujahr:"2019", Kilometer:"45.000", Preis:"29.000 €", Bild:mercedes},
+    {Hersteller:"Opel", Modell:"Corsa", PS:"69", Farbe:"Weiß", Baujahr:"2009", Kilometer:"90.000", Preis:"3.000 €", Bild:opel},
+    {Hersteller:"Bentley", Modell:"Bentayga", PS:"450", Farbe:"Schwarz", Baujahr:"2019", Kilometer:"59.000", Preis:"130.000 €", Bild:bentley},
+  ]
+
+  export default autoliste;
--- src/index.js
+++ src/index.js
@@ -1,57 +1,21 @@
-import {React, ReactDOM, CarListing, bmw, mercedes, opel, bentley} from './import.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CarListing from './components/carListing';
+import autoliste from './components/autoliste.js';
+import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <div className="container">
-    <div className="kachel">
-    <CarListing
-      Hersteller="BMW"
-      Modell="120i"
-      PS="170"
-      Farbe="Schwarz"
-      Baujahr="2007"
-      Kilometer="185.000"
-      Preis="6.700 €"
-      Bild={bmw}
-    />
-    </div>
-    <div className="kachel">
-    <CarListing
-      Hersteller="Mercedes"
-      Modell="A 250"
-      PS="224"
-      Farbe="Schwarz"
-      Baujahr="2019"
-      Kilometer="45.000"
-      Preis="29.000 €"
-      Bild={mercedes}
-    />
-    </div>
-    <div className="kachel">
-    <CarListing
-      Hersteller="Opel"
-      Modell="Corsa"
-      PS="69"
-      Farbe="Weiß"
-      Baujahr="2009"
-      Kilometer="90.000"
-      Preis="3.000 €"
-      Bild={opel}
-    />
-    </div>
+
+    <div className="container">
+    {autoliste.map((element, index) =>
       <div className="kachel">
-      <CarListing
-      Hersteller="Bentley"
-      Modell="Bentayga"
-      PS="450"
-      Farbe="Schwarz"
-      Baujahr="2019"
-      Kilometer="59.000"
-      Preis="130.000 €"
-      Bild={bentley}
-    />
+      <CarListing key={index} Hersteller={element.Hersteller} Modell={element.Modell} PS={element.PS} Farbe={element.Farbe} Baujahr={element.Baujahr} Kilometer={element.Kilometer} Preis={element.Preis} Bild={element.Bild}>
+      </CarListing>
+      </div>
+    )}
     </div>
 
-  </div>
 );
